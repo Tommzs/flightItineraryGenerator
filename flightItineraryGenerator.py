0d1f957a6256d25811ccdd81afffde58750f8522
Tomas Novak <tomasnovakmail@gmail.com>
2019-06-11 23:52:32 +0200

    Added time handling and directed graph generation

--- flightItineraryGenerator.py
+++ flightItineraryGenerator.py
@@ -1,10 +1,13 @@
 import sys
 import argparse
-
-### Class definitions
+from datetime import datetime
 from typing import List, Any
 
+### Constants
+HOUR_IN_SECONDS = 1*60*60
 
+
+### Class definitions
 class flightItinerary:
     """ flightItinerary represents one flight itinerary"""
     def __init__(self):
@@ -12,9 +15,25 @@ class flightItinerary:
         self.visitedAirports = []
         self.departure = None
         self.arrival = None
-        self.prices = None
+        self.price = None
         self.bags_allowed = None
-        self.bags_prices = None
+        self.bag_price = None
+
+    def get_price_with_bags(self, num_bags):
+        if self.bags_allowed is None:
+            print("ERROR: Bags allowed is not filled", file=sys.stderr)
+            return None
+        elif self.bag_price is None:
+            print("ERROR: Bags price is not filled", file=sys.stderr)
+            return None
+        elif self.price is None:
+            print("ERROR: Price is not filled", file=sys.stderr)
+            return None
+        elif num_bags > self.bags_allowed:
+            print("ERROR: Maximum bags allowed is {} which is less than {}".format(self.bags_allowed, num_bags), file=sys.stderr)
+            return None
+        else:
+            return self.price+num_bags*self.bag_price
 
 class flightSegment:
     """ flightSegment represents one flight segment"""
@@ -29,7 +48,7 @@ class flightSegment:
         self.bags_price = bag_price
 
 
-### public functions
+### Public functions
 def read_args():
     parser = argparse.ArgumentParser(description='Generate flight itineraries from input segments.')
     parser.add_argument('-i', '--input', required=False, help='input csv with flight segments', default='None')
@@ -38,21 +57,33 @@ def read_args():
     return args['input'], args['output']
 
 
+def str_to_datetime(time_str):
+    date_str, time_str = time_str.split('T')
+    year_str, month_str, day_str = date_str.split('-')
+    hour_str, minute_str, second_str = time_str.split(':')
+    return datetime(year=int(year_str),
+                    month=int(month_str),
+                    day=int(day_str),
+                    hour=int(hour_str),
+                    minute=int(minute_str),
+                    second=int(second_str))
+
+
 def process_line(line, header):
     line = line.rstrip('\n').split(',')
-    # TODO: Parse departure and arrival to extract date and time
-    fl_segm = flightSegment(line[header.index('source')],
-                           line[header.index('destination')],
-                           line[header.index('departure')],
-                           line[header.index('arrival')],
-                           float(line[header.index('price')]),
-                           int(line[header.index('bags_allowed')]),
-                           float(line[header.index('bag_price')]))
+    dep_time_str = line[header.index('departure')]
+    arr_time_str = line[header.index('arrival')]
+    fl_segm = flightSegment(source=line[header.index('source')],
+                           destination=line[header.index('destination')],
+                           departure=str_to_datetime(dep_time_str),
+                           arrival=str_to_datetime(arr_time_str),
+                           price=float(line[header.index('price')]),
+                           bags_allowed=int(line[header.index('bags_allowed')]),
+                           bag_price=float(line[header.index('bag_price')]))
     return fl_segm
 
 
 def read_input_data(file_path=None):
-    header: List[str] = []
     flight_segments: List[flightSegment] = []
 
     if file_path is None:
@@ -65,11 +96,32 @@ def read_input_data(file_path=None):
             for line in in_file:
                 flight_segments.append(process_line(line, header))
 
-    return flight_segments, header
+    return flight_segments
+
+
+def generate_graph(segments: List[flightSegment]):
+    directed_graph = dict()
+    for key_flight in segments:
+        directed_graph[key_flight] = set()
+        for next_flight in segments:
+            if key_flight.destination == next_flight.source:
+                time_diff = (next_flight.departure-key_flight.arrival).seconds
+                if HOUR_IN_SECONDS < time_diff < (4*HOUR_IN_SECONDS):
+                    directed_graph[key_flight].add(next_flight)
+    return directed_graph
+
+
+def generate_itineraries(flight_segs, flight_graph):
+    #TODO: Do DFS with parent map generating set of itineraries once there is nowhere to explore
+    None
 
 
 if __name__ == "__main__":
     # Read args, allows input file as parameter
     in_file_path, out_file_path = read_args()
     # Parse input data
-    flight_segments, header = read_input_data(in_file_path)
+    flight_segs = read_input_data(in_file_path)
+    flight_graph = generate_graph(flight_segs)
+    itineraries = []
+    for flight_seg in flight_segs:
+        generate_itineraries(flight_seg, flight_graph)
